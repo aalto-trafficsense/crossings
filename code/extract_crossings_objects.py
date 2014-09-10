76fab9db1c25ae562ce469abe2b7afe2aecbff58
Seppo Törmä <seppo.torma@aalto.fi>
2014-09-10 17:56:00 +0300

    Added clustering of crossings

--- code/extract_crossings_objects.py
+++ code/extract_crossings_objects.py
@@ -3,22 +3,30 @@ try:
 except ImportError as e:
     from xml.etree import ElementTree as ET
 
+import math
+import copy
+
 roadtypes = {'motorway', 'trunk', 'primary', 'secondary', 'tertiary',
              'unclassified', 'residential', 'minor', 'service'
              }
 
-def roadp(element):
+def isRoad(element):
     if element.tag == 'way':
         for item in element:
             if (item.tag == 'tag' and item.attrib['k']=="highway"):
                 return item.attrib['v'] in roadtypes
     return False
 
-def namep(element):
-    if element.tag == 'way':
-        for item in element:
-            if (item.tag == 'tag' and item.attrib['k']=="name" and len(item.attrib['v'])>1):
-                return True
+def wayName(way):
+    if way.tag == 'way':
+       for item in way:
+           if item.tag == 'tag' and item.attrib['k']=="name":
+              return item.attrib['v']
+        
+def wayHasName(element):
+    n = wayName(element)
+    if n != None:
+        return len(n)>1
     return False
 
 def node_increment(node_id, way):
@@ -34,38 +42,90 @@ def node_increment(node_id, way):
     else:
         return 2
 
+def pointId(lat, lon):
+    return str(math.floor(10000*lat))+str(math.floor(10000*lon))
+
 # Class for storing the detected crossings prior to clustering
 class Crossing:
+      id = None
+      lat = 0.0
+      lon = 0.0
+      ways = {}
       counter = 0; # Number of crossing ways weighted with endpoints
       node = None
-      ways = []
-      def __init__(self, id, way1, way2):
-          self.ways = []
-          self.addroad(way1, id)
-          self.addroad(way2, id)
+      fake = False # Crossing with only one road name (continuation of same road, division into two unidirectionals)
+      obsoletedBy = None
+      replacesCrossings = []
+      def __init__(self, id, lat, lon, ways):
+            self.id = id
+            self.lat = lat
+            self.lon = lon
+            self.ways = set()
+            for w in ways:
+               self.addroad(w, id)
+            self.fake = self.fakeCrossing()
       def addroad(self, way, id):
-          self.ways.append(way)
+          self.ways.add(way)
           self.counter += node_increment(id, way)
       def addnode(self, node):
           self.node = node
-      def lat(self):
-          return self.node.attrib['lat']
-      def long(self):
-          # print self.node.attrib['long']
-          return self.node.attrib['lon']
+          self.lat = node.attrib['lat']
+          self.lon = node.attrib['lon']
       def coordinate_string(self):
-          return self.lat() + ',' + self.long()
+          return str(self.lat) + ',' + str(self.lon)
       def properCrossing(self):
-          return self.counter > 2
-      def closeBy(self, other, e = 0.0001):
-          return abs(float(self.lat()) - float(other.lat())) < e and abs(float(self.long()) - float(other.long())) < e
+#          return self.counter > 2
+#          return self.counter > 1 and not self.fakeCrossing()
+          return self.counter > 1
+      def closeBy(self, other, e = 0.00018):
+          return abs(float(self.lat) - float(other.lat)) < e and abs(float(self.lon) - float(other.lon)) < e
       def closeByCrossings(self, crossings):
           result = []
           for c in crossings:
               cr = crossings[c]
-              if cr != self and self.closeBy(cr):
+              if cr != self and cr.obsoletedBy == None and not cr.fakeCrossing() and self.closeBy(cr):
                   result.append(cr)
-          return result         
+          return result
+      def wayNames(self):
+          return set(map(wayName, self.ways))
+      def printCoordinates(self, e="\n"):
+          print(self.coordinate_string(), end=e)
+      def printCrossing(self, crossings):
+          if self.fakeCrossing():
+              print("Fake crossing: ", end='')
+          self.printCoordinates(e='')
+          print(self.wayNames())
+      def fakeCrossing(self):
+          return len(self.wayNames()) == 1
+      def clusterableWith(self, crossing):
+          return len(self.wayNames() & crossing.wayNames()) > 1
+      def clusterCrossingWith(self, crossing, crossingMap, newCrossingMap):
+            print('Trying to cluster: ', end='')
+            self.printCrossing(crossingMap)
+            print('with the crossing: ', end='')
+            crossing.printCrossing(crossingMap)
+            if self.obsoletedBy == None and not self.fakeCrossing() and crossing.obsoletedBy == None and not crossing.fakeCrossing():
+                lat = (float(self.lat) + float(crossing.lat)) / 2
+                lon = (float(self.lon) + float(crossing.lon)) / 2
+                id = pointId(lat, lon)
+                c = Crossing(id, lat, lon, self.ways.union(crossing.ways))
+                print('Clustering result: ', end='')
+                c.printCrossing(crossingMap)
+                print('--')
+                newCrossingMap[id] = c
+                self.obsoletedBy = c
+                crossing.obsoletedBy = c
+                return c
+            return self
+      def clusterCrossing(self, crossingMap, newCrossingMap):
+            if self.obsoletedBy == None and not self.fakeCrossing():
+                for n in self.closeByCrossings(crossingMap):
+                    if self.clusterableWith(n):
+                        c = self.clusterCrossingWith(n, crossingMap, newCrossingMap)
+                        return c.clusterCrossing(crossingMap, newCrossingMap)
+                    else:
+                        return self
+      
     
 def extract_intersections(osm, verbose=True):
     # This function takes an osm file as an input. It then goes through each xml 
@@ -81,51 +141,49 @@ def extract_intersections(osm, verbose=True):
     encountered = {} # What kind of nodes have been encountered
     crossings = {}
     for child in root:
-        if roadp(child) and namep(child):
+        if isRoad(child) and wayHasName(child):
            for item in child:
                if item.tag == 'nd':
                   nd_ref = item.attrib['ref']
                   if not nd_ref in encountered:    # Encountered the first time 
                       encountered[nd_ref] = child
                   elif not nd_ref in crossings: # Encountered the second time
-                      crossings[nd_ref] = Crossing(nd_ref, child, encountered[nd_ref])
+                      crossings[nd_ref] = Crossing(nd_ref, 0.0, 0.0, {child, encountered[nd_ref]})
                   else:                             # After the second time
                       crossings[nd_ref].addroad(child, item)
     for child in root:
         if child.tag == "node" and child.attrib['id'] in crossings:
             crossings[child.attrib['id']].addnode(child)
-
-    # Find nodes that are shared with more than one way, which
-    # might correspond to intersections
-
-    #intersections = filter(lambda x: counter[x] > 2,  counter)
+            
     crossings = dict(filter(lambda k: crossings[k[0]].properCrossing(), crossings.items()))
 
-    # Extract intersection coordinates
-    # You can plot the result using this url.
-    # http://www.darrinward.com/lat-long/
-    print(len(crossings))
+#    print(len(crossings))
     return crossings
+
+def cluster_crossings(crossings):
+    clusteredCrossings = {}
+    for item in crossings.items():
+        item[1].clusterCrossing(crossings, clusteredCrossings)
+    return clusteredCrossings
 
 def print_coordinates(osm):
     c = extract_intersections(osm)
+    clusters = cluster_crossings(c)
+    print('Crossings')
     for id in c:
-        print(c[id].coordinate_string())
+        c[id].printCoordinates()
+    print('Clustered crossings')
+    for id in clusters:
+          if clusters[id].obsoletedBy == None:
+             clusters[id].printCoordinates()
+
 
 def print_crossings(osm):
     c = extract_intersections(osm)
     for id in c:
-        print(c[id].coordinate_string() + ' - ' + str(len(c[id].ways)) + '(' +str(c[id].counter) + ')')
-        for w in c[id].ways:
-                for t in w:
-                    if t.tag == 'tag' and t.attrib['k'] == 'name':
-                        print(' %r' % t.attrib['v'])
+        c[id].printCrossing(c)
         for n in c[id].closeByCrossings(c):
-            print('   %r' % n)
-            for w in n.ways:
-                for t in w:
-                    if t.tag == 'tag' and t.attrib['k'] == 'name':
-                        print('        %r' % t.attrib['v'])
+            n.printCrossing(c)
         
 print_coordinates("../data/otaniemi.osm")
 
