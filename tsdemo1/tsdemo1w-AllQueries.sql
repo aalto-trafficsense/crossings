01517c216bdd3f04a423fd65e5180cfddb60b75e
Joonas Javanainen <joonas.javanainen@aalto.fi>
2014-12-10 19:15:33 +0200

    Replace invalid NOT LIKE predicate with NOT IN
    LIKE is meant for substring matches, and should not be used for equality
    comparison (= LIKE without any % is meaningless). Also, instead of
    repeated NOT LIKE expressions it is much cleaner to use NOT IN.
    
    Performance impact (Finland data set):
    
    Before:
      (cost=179690.64..186700.80 rows=560813 width=261)
      (actual time=712.938..994.897 rows=368164 loops=1)
    
    After:
      (cost=162562.30..169023.89 rows=516927 width=261)
      (actual time=658.120..938.624 rows=368164 loops=1)

--- tsdemo1/tsdemo1w-AllQueries.sql
+++ tsdemo1/tsdemo1w-AllQueries.sql
@@ -37,10 +37,13 @@
 DROP TABLE IF EXISTS roadslist;
 
 CREATE TABLE roadslist AS
-SELECT DISTINCT osm_id as road_id, name, oneway, way as geom
-FROM planet_osm_line
-WHERE (highway NOT  LIKE 'cycleway') AND (highway NOT LIKE 'footway') AND (highway NOT LIKE 'pedestrian') AND (highway NOT LIKE 'steps') AND (highway NOT  LIKE 'service') AND (highway NOT LIKE 'path') AND (highway NOT LIKE 'platform') AND (highway NOT LIKE 'construction');
-
+  SELECT DISTINCT
+    osm_id as road_id, name, oneway, way as geom
+  FROM
+    planet_osm_line
+  WHERE
+    highway NOT IN ('cycleway', 'footway', 'pedestrian', 'steps', 'service', 'path', 'platform', 'construction')
+;
 
 
 /* CreateRoadsNodesInOrder */
