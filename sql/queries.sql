d2e9cee848504f58a162442084886320c160394e
Joonas Javanainen <joonas.javanainen@aalto.fi>
2015-01-12 19:35:33 +0200

    Import roadslist queries

---
+++ sql/queries.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS roadslist;
+
+CREATE TABLE roadslist AS
+SELECT DISTINCT osm_id as road_id, name, oneway, way as geom
+FROM planet_osm_line
+WHERE (highway NOT  LIKE 'cycleway') AND (highway NOT LIKE 'footway') AND (highway NOT LIKE 'pedestrian') AND (highway NOT LIKE 'steps') AND (highway NOT  LIKE 'service') AND (highway NOT LIKE 'path') AND (highway NOT LIKE 'platform') AND (highway NOT LIKE 'construction');
