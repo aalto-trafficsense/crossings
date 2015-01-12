554f9e1e56d240711ca588bd004dce1ed5bc8287
Joonas Javanainen <joonas.javanainen@aalto.fi>
2015-01-12 19:41:48 +0200

    Immediately map oneway to a boolean
    We do this later anyway, so storing the intermediate string
    representation is pointless.

--- sql/queries.sql
+++ sql/queries.sql
@@ -2,7 +2,10 @@ DROP TABLE IF EXISTS roads;
 
 CREATE TABLE roads AS
   SELECT DISTINCT
-    osm_id as road_id, name, oneway, way as geom
+    osm_id as road_id,
+    name,
+    CASE WHEN oneway = 'yes' THEN true ELSE false END AS oneway,
+    way as geom
   FROM
     planet_osm_line
   WHERE
