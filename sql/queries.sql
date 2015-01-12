c570b38cc6b207d534243cfb976693a5350fe945
Joonas Javanainen <joonas.javanainen@aalto.fi>
2015-01-12 19:49:24 +0200

    Require distinct ids, not distinct rows
    Distinct by id is faster (index scan) and enables us to set up a primary key.
    
    Note: This will drop duplicate rows silently (= the first row with
    certain id wins), but in Finland data there is only a couple of cases.
    If this becomes a problem, we'll have to figure out what is the exact
    meaning of duplicate ids in the data and handle those cases in some
    way.

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS roads;
 
 CREATE TABLE roads AS
-  SELECT DISTINCT
-    osm_id as road_id,
+  SELECT
+  DISTINCT ON (osm_id)
+    osm_id as id,
     name,
     CASE WHEN oneway = 'yes' THEN true ELSE false END AS oneway,
     way as geom
