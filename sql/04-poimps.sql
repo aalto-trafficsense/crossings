9c036b08f562097b0726c56e240acd89bb6c12c6
Joonas Javanainen <joonas.javanainen@aalto.fi>
2015-03-01 19:40:43 +0200

    Add 'poimps' queries

---
+++ sql/04-poimps.sql
@@ -0,0 +1,48 @@
+-- The poimps table contains points of importance, which are
+-- nodes that we are interested in
+
+DROP TABLE IF EXISTS poimps;
+
+CREATE UNLOGGED TABLE poimps (
+  node_id  bigint PRIMARY KEY,
+  geometry geometry(point, 3857) NOT NULL
+);
+
+INSERT INTO poimps
+  WITH nodes AS (
+    -- Complex intersections
+    SELECT node_id
+    FROM roads_nodes
+    GROUP BY node_id
+    HAVING COUNT(road_id) > 2
+
+    UNION
+
+    -- Geometrical dead ends
+    SELECT node_id
+    FROM roads_nodes
+    GROUP BY node_id
+    -- bool_or is needed, because we are interested in nodes that are
+    -- endpoints in *at least one* road
+    HAVING COUNT(road_id) = 1 AND bool_or(is_endpoint) IS TRUE
+
+    UNION
+
+    -- Simple intersections
+    SELECT node_id
+    FROM roads_nodes
+    GROUP BY node_id
+    -- bool_or is needed, because we are interested in nodes that are not
+    -- endpoints in *both* roads
+    HAVING COUNT(road_id) = 2 AND bool_and(is_endpoint) IS FALSE
+  )
+  SELECT
+    distinct_nodes.node_id,
+    -- 100 is some kind of scaling factor. lon and lat are integers in the planet_osm_nodes table
+    ST_SetSRID(ST_MakePoint(lon::double precision / 100, lat::double precision / 100), 3857)
+  FROM (SELECT DISTINCT node_id FROM nodes) AS distinct_nodes
+  JOIN planet_osm_nodes
+  ON id = node_id
+;
+
+CREATE INDEX ON poimps USING GIST(geometry);
--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,4 @@
 \ir 01-highwaymodes.sql
 \ir 02-roads.sql
 \ir 03-roads_nodes.sql
+\ir 04-poimps.sql
