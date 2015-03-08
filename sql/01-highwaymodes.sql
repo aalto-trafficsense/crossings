ee94db9cc5f30015046286fc37053be47c995b51
Joonas Javanainen <joonas.javanainen@aalto.fi>
2015-03-08 23:11:17 +0200

    Include secondary_link highways

--- sql/01-highwaymodes.sql
+++ sql/01-highwaymodes.sql
@@ -15,24 +15,25 @@ CREATE UNLOGGED TABLE highwaymodes (
 -- distinquish between true and false values
 -- TODO: Remove unused rows such as elevator
 INSERT INTO highwaymodes
-  (highway,         motorcar, bicycle, foot)
+  (highway,          motorcar, bicycle, foot)
 VALUES
-  ('access',        'YES',    'YES',   'YES'),
-  ('cycleway',      'no',     'YES',   'YES'),
-  ('elevator',      'no',     'no',    'YES'),
-  ('footway',       'no',     'YES',   'YES'),
-  ('living_street', 'YES',    'YES',   'YES'),
-  ('motorway',      'YES',    'no',    'no'),
-  ('motorway_link', 'YES',    'no',    'no'),
-  ('path',          'no',     'YES',   'YES'),
-  ('pedestrian',    'no',     'YES',   'YES'),
-  ('primary',       'YES',    'YES',   'YES'),
-  ('primary_link',  'YES',    'YES',   'YES'),
-  ('residential',   'YES',    'YES',   'YES'),
-  ('secondary',     'YES',    'YES',   'YES'),
-  ('steps',         'no',     'no',    'YES'),
-  ('tertiary',      'YES',    'YES',   'YES'),
-  ('tertiary_link', 'YES',    'YES',   'YES'),
-  ('trunk',         'YES',    'no',    'no'),
-  ('track',         'no',     'YES',   'YES'),
-  ('unclassified',  'YES',    'no',    'no');
+  ('access',         'YES',    'YES',   'YES'),
+  ('cycleway',       'no',     'YES',   'YES'),
+  ('elevator',       'no',     'no',    'YES'),
+  ('footway',        'no',     'YES',   'YES'),
+  ('living_street',  'YES',    'YES',   'YES'),
+  ('motorway',       'YES',    'no',    'no'),
+  ('motorway_link',  'YES',    'no',    'no'),
+  ('path',           'no',     'YES',   'YES'),
+  ('pedestrian',     'no',     'YES',   'YES'),
+  ('primary',        'YES',    'YES',   'YES'),
+  ('primary_link',   'YES',    'YES',   'YES'),
+  ('residential',    'YES',    'YES',   'YES'),
+  ('secondary',      'YES',    'YES',   'YES'),
+  ('secondary_link', 'YES',    'YES',   'YES'),
+  ('steps',          'no',     'no',    'YES'),
+  ('tertiary',       'YES',    'YES',   'YES'),
+  ('tertiary_link',  'YES',    'YES',   'YES'),
+  ('trunk',          'YES',    'no',    'no'),
+  ('track',          'no',     'YES',   'YES'),
+  ('unclassified',   'YES',    'no',    'no');
